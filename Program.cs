21a1587b3c9aefa1b4e45b27ff4d36c755e2c1be
cstojcevski <79820146+cstojcevski@users.noreply.github.com>
2021-04-02 23:02:01 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int integer = 1; integer <= 100; integer++)  {
+            
+            if (integer % 3==0) {
+            Console.WriteLine("Fizz"); }
+
+            if (integer % 5==0) {
+            Console.WriteLine("Buzz"); }
+
+            if (integer % 15==0) {
+            Console.WriteLine("Fizzbuzz"); }
+            
+            else {Console.WriteLine(integer);}
+
+            } 
+        }
+    }
+}
